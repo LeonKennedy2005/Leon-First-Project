9ffe0a4573eab13cce0cb378c5869c051ae33bf8
LeonKennedy2005 <139060906+LeonKennedy2005@users.noreply.github.com>
2023-07-12 08:53:53 +0600

    Added basic iText funsctions

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.Leon;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LeonApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {

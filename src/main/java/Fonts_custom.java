a23f99d82285a989baf158518f514dbfbd348fa5
LeonKennedy2005 <139060906+LeonKennedy2005@users.noreply.github.com>
2023-07-13 20:12:31 +0600

    Added images, a listand custom fonts
    -

---
+++ src/main/java/Fonts_custom.java
@@ -0,0 +1,37 @@
+import com.itextpdf.io.font.FontConstants;
+import com.itextpdf.kernel.font.PdfFont;
+import com.itextpdf.kernel.font.PdfFontFactory;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Paragraph;
+import com.itextpdf.layout.element.Text;
+
+import java.io.IOException;
+
+public class Fonts_custom {
+    public static final String Pacifico = "fonts\\Pacifico.ttf";
+    public static final String OpenSansB = "fonts\\OpenSans-Bold.ttf";
+
+    public static void main(String[] args) throws IOException
+    {
+        String path = "C:\\Users\\leonk\\Desktop\\Results\\FontsCustom.pdf";
+        PdfWriter pdfWriter = new PdfWriter(path);
+        PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+        Document document = new Document(pdfDocument);
+
+        PdfFont Pacifico1 = PdfFontFactory.createFont(Pacifico,true );
+        PdfFont OpenSansB1 = PdfFontFactory.createFont(OpenSansB, true);
+
+
+        Text text1 = new Text("Brown fox jumped over lazy dog").setFont(Pacifico1);
+        Text text2 = new Text("Brown fox jumped over lazy dog").setFont(OpenSansB1);
+
+        Paragraph paragraph =new Paragraph().add(text1).add(text2);
+
+        document.add(paragraph);
+        document.close();
+        System.out.println("PDF created");
+
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,49 @@
+import com.itextpdf.io.image.ImageData;
+import com.itextpdf.io.image.ImageDataFactory;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.element.List;
 import com.itextpdf.layout.element.Paragraph;
-
 import java.io.FileNotFoundException;
-import java.util.Arrays;
+
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        String path = "C:\\Users\\leonk\\Desktop\\Results\\FirstPdf.pdf";
+        //text
+        String path = "C:\\Users\\leonk\\Desktop\\Results\\Text-Image-List.pdf";
         String paraText = "What is Lorem Ipsum?\n" +
-                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+                "Body text, nothing to see here";
+        //
         Paragraph paragraph1=new Paragraph(paraText);
         PdfWriter pdfWriter = new PdfWriter(path);
         PdfDocument pdfDocument = new PdfDocument(pdfWriter);
         pdfDocument.addNewPage();
         Document document = new Document(pdfDocument);
         document.add(paragraph1);
-        document.close();
+        //image
+        try {
 
-        System.out.println("Hello World");
+            String XboxImage = "images\\Xbox.jpg";
+            ImageData data = ImageDataFactory.create(XboxImage);
+            Image Xbox = new Image(data);
+            float desiredWidth = 197;
+            float desiredHeight = 111;
+            Xbox.scaleToFit(desiredWidth, desiredHeight);
+            document.add(Xbox);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }//
+
+        List programming_lan = new List();
+        programming_lan.add("Python");
+        programming_lan.add("Java");
+        programming_lan.add("C");
+        programming_lan.add("JavaScrpit");
+        document.add(programming_lan);
+        document.close();
+        System.out.println("PDF created");
     }
 }

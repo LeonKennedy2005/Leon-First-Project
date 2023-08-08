8133a88bd1a685132facaf7c5925f3fd9b90f941
LeonKennedy2005 <139060906+LeonKennedy2005@users.noreply.github.com>
2023-08-08 13:58:30 +0600

    Partial implementation of swagger

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
+@Import(SwaggerConfig.class) // Import the SwaggerConfig explicitly
 public class DemoApplication {
 
 	public static void main(String[] args) {
